7ce503cb2049e0b064476e4939c4499ac0dfbeb3
uncleoptimus <uncle.optimus@gmail.com>
2015-05-12 12:08:54 -0700

    added PropertyView and geolocation functionality

--- SearchPage.js
+++ SearchPage.js
@@ -97,6 +97,21 @@ class SearchPage extends Component {
     this.setState({ searchString: event.nativeEvent.text });
   }
 
+  onLocationPressed() {
+    navigator.geolocation.getCurrentPosition(
+      location => {
+        var search = location.coords.latitude + ',' + location.coords.longitude;
+        this.setState({ searchString: search });
+        var query = urlForQueryAndPage('centre_point', search, 1);
+        this._executeQuery(query);
+      },
+      error => {
+        this.setState({
+          message: 'There was a problem with obtaining your location: ' + error
+        });
+      });
+  }
+
   _executeQuery(query) {
     console.log(query);
     this.setState({ isLoading: true });
@@ -154,7 +169,8 @@ class SearchPage extends Component {
           </TouchableHighlight>
         </View>
         <TouchableHighlight style={styles.button}
-          underlayColor='#99d9f4'>
+          underlayColor='#99d9f4'
+          onPress={this.onLocationPressed.bind(this)}>
           <Text style={styles.buttonText}>Location</Text>
         </TouchableHighlight>
         <Image source={require('image!house')} style={styles.image}/>
--- SearchResults.js
+++ SearchResults.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var React = require('react-native');
+var React = require('react-native'),
+    PropertyView = require('./PropertyView');
 var {
   StyleSheet,
   Image,
@@ -52,6 +53,12 @@ class SearchResults extends Component {
 
   rowPressed(propertyGuid) {
     var property = this.props.listings.filter(prop => prop.guid === propertyGuid)[0];
+
+    this.props.navigator.push({
+      title: 'Property',
+      component: PropertyView,
+      passProps: {property: property}
+    });
   }
 
   renderRow(rowData, sectionID, rowID) {
